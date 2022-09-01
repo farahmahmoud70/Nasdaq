edab29f4f1ea783a033e405a740383add52d74b5
farah.mahmoud <Farah.Mahmoud@qsitint.com>
2022-09-01 15:36:04 +0200

    [feat]: Nasdaq: handle adding routes

--- nasdaq/src/App.tsx
+++ nasdaq/src/App.tsx
@@ -1,17 +1,17 @@
 import './App.css';
 import { Layout } from './app/components/layout/Layout';
 import Header from './app/components/header/Header';
-import MainSection from './app/components/mainSection/MainSection';
 import Footer from './app/components/footer/Footer';
 import { GlobalStyles } from './app/style/Global';
+import AppRoutes from './Routes';
 
-function App() {
+const App = () => {
 
   return <>
     <GlobalStyles />
     <Layout>
       <Header />
-      <MainSection />
+      <AppRoutes />
       <Footer />
     </Layout>
   </>
