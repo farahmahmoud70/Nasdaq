020723b1d7849cc9975459976a892accb7e1c013
farah.mahmoud <Farah.Mahmoud@qsitint.com>
2022-09-03 16:39:37 +0200

    [fix]: Nasdaq: handle apiKey from the request header inside of being query

--- nasdaq/src/app/handlers/stocksApiHandler.ts
+++ nasdaq/src/app/handlers/stocksApiHandler.ts
@@ -6,26 +6,29 @@ const getTickerDetailsUrl = `${appConfig.polygonUrl}${appConfig.services.tickerD
 const getTickerDailyDetailsUrl = `${appConfig.polygonUrl}${appConfig.services.tickerDailyDetails}`;
 const stockSearchUrl = `${appConfig.polygonUrl}${appConfig.services.stockSearch}`;
 
+const auth = `Bearer ${appConfig.apiKey}`;
+
 const getStocks = (nextUrl?: string) => {
-  return axios.get(
-    nextUrl
-      ? `${nextUrl}&apiKey=${appConfig.apiKey}`
-      : getStocksUrl.replace('{0}', appConfig.apiKey)
-  );
+  return axios.get(nextUrl ? nextUrl : getStocksUrl, {
+    headers: {
+      Authorization: auth,
+    },
+  });
 };
 const getTickerDetails = (ticker: string) => {
-  return axios.get(
-    getTickerDetailsUrl.replace('{0}', ticker).replace('{1}', appConfig.apiKey)
-  );
+  return axios.get(getTickerDetailsUrl.replace('{0}', ticker), {
+    headers: {
+      Authorization: auth,
+    },
+  });
 };
 const getTickerDailyDetails = (ticker: string, date: string) => {
   return axios
-    .get(
-      getTickerDailyDetailsUrl
-        .replace('{0}', ticker)
-        .replace('{1}', date)
-        .replace('{2}', appConfig.apiKey)
-    )
+    .get(getTickerDailyDetailsUrl.replace('{0}', ticker).replace('{1}', date), {
+      headers: {
+        Authorization: auth,
+      },
+    })
     .catch((err) => ({
       data: { tickerDailyDetailsError: err.response.data.message },
     }));
@@ -34,9 +37,12 @@ const stockSearch = (searchTerm: string, nextSearchUrl?: string) => {
   return axios.get(
     nextSearchUrl
       ? `${nextSearchUrl}&apiKey=${appConfig.apiKey}`
-      : stockSearchUrl
-          .replace('{0}', searchTerm)
-          .replace('{1}', appConfig.apiKey)
+      : stockSearchUrl.replace('{0}', searchTerm),
+    {
+      headers: {
+        Authorization: auth,
+      },
+    }
   );
 };
 const polygonHandler = {
